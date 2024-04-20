46fea7420e68f5537caff0db1082582683d53347
sam-gmarvis <sam@ethos-cm.com>
2024-04-20 17:29:04 +0100

    solving Greatest common divisor on leetcode

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-// Leetcode ====> Merge Strings Alternately
+// solution
+// 1. convert both strings into an array
+// 2. create a variable and assign it to an empty array to store the divisor characters
+// 3. iterate over both arrays
+// 4. check if letter at first array match that of second array at same index then push leter in the divisor array
+// 5.
 
-// Psudo code
-// ===> steps <====
+var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) return "";
 
-// 1. convert the two string into and array of characters eg arr1 and arr2
-// 2. define a varaible called merged array to store merged characters
-// 3. difine a variable called longArray
-// 3. check if both arrays of the same size is yes continue to step 4 else set longArray to take the longer array
-// 4. iterate over but array push each character to our defined mergedArray stating with the first arr or the first sting in the params
-// 5. convert the mergedArray back to a string and return it
+  let divisor = [];
+  let start = 0;
+  let end = 0;
+  let arr1 = str1.split("");
+  let arr2 = str2.split("");
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      if (divisor.includes(arr1[i]))
+        return divisor.toString().split(",").join("");
+      divisor.push(arr2[i]);
+      end += 1;
+    }
+  }
+
+  return divisor.toString().split(",").join("");
+};
+// console.log(gcdOfStrings("ABABAB", "ABAB"));
+
+// the above metthod is failing one test case
+
+// method 2 Using RECURSION
+// steps
+// 1. check if str1 + str2 !== str2 + str1 then we return an emty string
+// 2. check if str1 == str2 then we return str1
+// 3. check if the length of str1 > length of str2 then we recurcively call our function passing a substring of str1 at a length of str2 and str2 as params
+// 4. wlse we return recurcively our function passing a substring of str2 at length of str1 and str1 as params
 
 // solution
 
-var mergeAlternately = function (word1, word2) {
-  let mergedArray = [];
-
-  let arr1 = word1.split("");
-  let arr2 = word2.split("");
-
-  let longArr;
-  if (arr1.length > arr2.length) {
-    longArr = arr1;
+var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) {
+    return "";
+  } else if (str1 == str2) {
+    return str1;
+  } else if (str1.length > str2.length) {
+    return gcdOfStrings(str1.substring(str2.length), str2);
   } else {
-    longArr = arr2;
+    return gcdOfStrings(str2.substring(str1.length), str1);
   }
-  for (let i = 0; i < longArr.length; i++) {
-    mergedArray.push(arr1[i]);
-    mergedArray.push(arr2[i]);
-  }
-  return mergedArray.toString().split(",").join("");
 };
-console.log(mergeAlternately("abc", "pqrzzzz"));
+
+console.log(gcdOfStrings("ABABAB", "ABAB")); // time comlexity Big O(n)
--- solution.md
+++ solution.md
@@ -4,14 +4,16 @@
 
 # Approach
 
+Recurtion
+
 <!-- Describe your approach to solving the problem. -->
 
-1. convert the two string into and array of characters eg arr1 and arr2
-2. define a varaible called merged array to store merged characters
-3. difine a variable called longArray
-4. check if both arrays of the same size is yes continue to step 4 else set longArray to take the longer array
-5. iterate over but array push each character to our defined mergedArray stating with the first arr or the first sting in the params
-6. convert the mergedArray back to a string and return it
+# steps
+
+1. check if str1 + str2 !== str2 + str1 then we return an emty string
+2. check if str1 == str2 then we return str1
+3. check if the length of str1 > length of str2 then we recurcively call our function passing a substring of str1 at a length of str2 and str2 as params
+4. wlse we return recurcively our function passing a substring of str2 at length of str1 and str1 as params
 
 # Complexity
 
@@ -23,27 +25,17 @@
 
 # Code
 
-```
-/**
- * @param {string} word1
- * @param {string} word2
- * @return {string}
- */
-var mergeAlternately = function (word1, word2) {
-  let mergedArray = [];
-  let arr1 = word1.split("");
-  let arr2 = word2.split("");
-
-  let longArr;
-  if (arr1.length > arr2.length) {
-    longArr = arr1;
+```var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) {
+    return "";
+  } else if (str1 == str2) {
+    return str1;
+  } else if (str1.length > str2.length) {
+    return gcdOfStrings(str1.substring(str2.length), str2);
   } else {
-    longArr = arr2;
+    return gcdOfStrings(str2.substring(str1.length), str1);
   }
-  for (let i = 0; i < longArr.length; i++) {
-    mergedArray.push(arr1[i]);
-    mergedArray.push(arr2[i]);
-  }
-  return mergedArray.toString().split(",").join("");
 };
+
+console.log(gcdOfStrings("ABABAB", "ABAB"));
 ```
