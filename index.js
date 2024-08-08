d2c94d3e268282f21b8b7000c8aca9d3edf88de5
gmarvis <sgmarvis@gmail.com>
2024-08-08 13:20:31 +0100

    Find the Index of the First Occurrence in a String

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
-var maxArea = function (height) {
-	// define two pointer left and right
-	// define a variable maxWater to store our results
-	// in while loop
-	// calculate the area between two pointer and update maxWater
-	// check the shortest pointer and move it toward the other pointer
+var strStr = function (haystack, needle) {
+    // **********pseudo code**********
 
-	let left = 0;
-	let right = height.length - 1;
-	let maxWater = 0
+    // 1- make a nested loop throw the string(haystack)
+    // where at every index of the string(haystack),
+    // we loop throw the target string(needle)
 
-	while (left < right) {
-		let length = Math.min(height[left], height[right]);
-		let width = right - left;
-		let area = length * width;
-		maxWater = Math.max(maxWater, area);
+    // --loop throw the string(haystack)
+    // defined a variable "isMatch" that defines wether the strings match or not
+    // -- ON the j loop we loop throw throw the target string(needle)
+    //     check if the chart at each index match at both index
+    //     if they match we set isMatch variable to true
+    //     if they don't match we set it to false and then break
+    // lastly we check its a match we return the index else we return -1
 
-		if (height[left] < height[right]) {
-			left += 1;
-		} else right -= 1;
-	}
+    if (!needle) return 0;
 
-	return maxWater;
+    for (let i = 0; i < haystack.length; i++) {
+        let isMatch = true;
+
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                isMatch = false;
+                break;
+            }
+        }
+        if (isMatch) return i;
+    }
+    return -1;
 };
 
-console.log(maxArea(height));
+// solution 2
+var strStrSolutionTwo = function (haystack, needle) {
+    // step 1 get the lengths of both strings
+    // step 2 check if the target length is === 0 and return 0
+    // Step 3: Loop through haystack up to the point where the remaining substring can fit needle
+    // Step 4: Check if the substring of haystack from the current index matches needle
+    // Step 5: If a match is found, return the current index
+    // Step 6: If no match is found, return -1
+    let htsLength = haystack.length;
+    let ntLength = needle.length;
+    if (ntLength === 0) return 0;
+    for (let i = 0; i <= htsLength - ntLength; i++) {
+        if (haystack.substring(i, ntLength) === needle) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+console.log(strStr("sadbutsad", ""));
+console.log(strStr("leetcode", "leeto"));
+console.log(strStrSolutionTwo("sadbutsad", ""));
+console.log(strStrSolutionTwo("leetcode", "leeto"));
